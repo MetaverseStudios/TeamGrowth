31f5b56edb6ea061d9a395025fa8446f6f07fc83
Metaverse Studios <108854088+MetaverseStudios@users.noreply.github.com>
2022-07-16 14:01:21 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import NavBar from './Components/NavBar'
+
+const App = (props) => {
+    
+    return (
+        <div>
+            <NavBar/>
+        </div>
+    )
+}
+
+export default App
